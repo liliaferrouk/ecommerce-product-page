b6c688a20abc6b6bf6d05c2025a3d9fe7382f941
liliaferrouk <ferrouklilia@gmail.com>
2024-05-03 20:00:56 +0200

    styling header 50%

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Header.css'
+import img_logo from './images/logo.svg'
+import img_cart from './images/icon-cart.svg'
+import img_avatar from './images/image-avatar.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <img className='img_logo' src={img_logo} alt="logo"/>
+      <ul>
+      <li><a href="index.html">Collections</a></li>
+      <li><a href="index.html">Men</a></li>
+      <li><a href="index.html">Women</a></li>
+      <li><a href="index.html">About</a></li>
+      <li><a href="index.html">Contact</a></li>
+      </ul>
+      <img className='img_cart' src={img_cart} alt="cart"/>
+      <img className='img_avatar' src={img_avatar} alt="cart"/>
+
+    </header>
   )
 }
 
