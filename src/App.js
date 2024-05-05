8c1dc330c9ece3bc830cdddc343279b248e8fdd2
liliaferrouk <ferrouklilia@gmail.com>
2024-05-05 15:15:09 +0200

    fixed darkmode prob

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Header'
 import AfficheImages from './AfficheImages'
 import AfficheText from './AfficheText'
@@ -8,6 +8,14 @@ function App() {
   const [cartVisible,setCartVisible] = useState(false)
   const[compteur,setCompteur] = useState(0)
   const[nbItems,setNbItems] = useState(0)
+  useEffect(() => {
+    if (navigator.userAgent.match(/samsung/i)) {
+      alert("Your browser (Samsung Internet) may not show this website" +
+            " correctly. Please consider using a standards-compliant" +
+            " browser instead. \n\n" +
+            "We recommend Firefox, Microsoft Edge, or Google Chrome.");
+    }
+  }, []);
   return (
     <div className='App'>
       <Header setCartVisible={setCartVisible} nbItems={nbItems}/>
